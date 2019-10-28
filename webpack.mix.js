108e77c58c32a27fb2330e94bafb2ce5a732517c
Casper <me@casperengelmann.com>
2019-10-28 11:12:53 +0100

    added tailwind

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,49 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+const atImport = require('postcss-import');
+const nested = require('postcss-nested');
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+require('laravel-mix-tailwind');
+require('laravel-mix-purgecss');
+require('laravel-mix-postcss-config');
+require('laravel-mix-svg');
+
+mix.copyDirectory('resources/img', 'public/img');
+
+mix.svg({
+  class: 'icon fill-current max-w-full h-auto',
+  assets: ['./resources/svg/'],
+  output: './resources/js/svg.js',
+});
+
+mix
+  .js('resources/js/app.js', 'public/js')
+  .postCss('resources/css/app.css', 'public/css')
+  .postCssConfig({
+    plugins: [
+      atImport(),
+      nested(),
+    ],
+  })
+  .tailwind('./tailwind.config.js');
+
+if (mix.inProduction()) {
+  const ASSET_URL = `${process.env.ASSET_URL}/`;
+
+  mix.webpackConfig((webpack) => ({
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.ASSET_PATH': JSON.stringify(ASSET_URL),
+      }),
+    ],
+    output: {
+      publicPath: ASSET_URL,
+    },
+  }));
+
+  mix
+    .version()
+    .purgeCss({
+      folders: ['resources', 'public/vendor/nova', 'resources/views/vendor/mail/html'],
+    });
+}
